edb5800109e5077708e3ba8ba1ae913ec4da3068
Anand Doshi <anand@erpnext.com>
2014-01-17 15:12:22 +0530

    title patch: use strip_html of webnotes framework

--- aapkamanch/patches/update_post_title_and_status.py
+++ aapkamanch/patches/update_post_title_and_status.py
@@ -1,6 +1,6 @@
 from __future__ import unicode_literals
 import webnotes
-from HTMLParser import HTMLParser
+from webnotes.utils import strip_html
 
 def execute():
 	webnotes.reload_doc("aapkamanch", "doctype", "post")
@@ -9,22 +9,7 @@ def execute():
 	
 	for name, content in webnotes.conn.sql("""select name, content from `tabPost`"""):
 		# extract text, strip and remove new lines and spaces
-		title = strip_tags(content)
+		title = strip_html(content)
 		title = title.strip().replace("\n", " ").replace("  ", " ")
 		title = title[:100] + ("..." if len(title) > 100 else "")
 		webnotes.conn.set_value("Post", name, "title", title)
-
-# http://stackoverflow.com/questions/753052/strip-html-from-strings-in-python
-class MLStripper(HTMLParser):
-	def __init__(self):
-		self.reset()
-		self.fed = []
-	def handle_data(self, d):
-		self.fed.append(d)
-	def get_data(self):
-		return ''.join(self.fed)
-
-def strip_tags(html):
-	s = MLStripper()
-	s.feed(html)
-	return s.get_data()
