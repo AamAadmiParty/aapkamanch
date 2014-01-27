9b6dd9e8022b0b6e53603243cf882acc07d09295
Anand Doshi <anand@erpnext.com>
2014-01-27 14:56:46 +0530

    Show Events and Popular Posts in Daily Summary

--- aapkamanch/summary.py
+++ aapkamanch/summary.py
@@ -2,6 +2,9 @@
 
 from __future__ import unicode_literals
 
+import os
+from inlinestyler.utils import inline_css
+
 import webnotes
 from webnotes.utils import today, add_days, getdate, get_datetime
 from webnotes.utils.email_lib.bulk import send
@@ -17,17 +20,20 @@ def send_daily_summary(for_date=None, event_date=None):
 	formatted_date = getdate(for_date).strftime("%a, %d %B, %Y")
 	formatted_event_date = getdate(event_date).strftime("%a, %d %B, %Y")
 	subject = "[AAP Ka Manch] Updates for {formatted_date}".format(formatted_date=formatted_date)
-	unit_post_map = get_unit_post_map(for_date, event_date)
+	posts, events = get_posts_and_events(for_date, event_date)
 	
-	if not unit_post_map:
+	if not (posts or events):
 		# no updates!
 		return
 		
 	for user in webnotes.conn.sql_list("""select name from `tabProfile`
 		where user_type='Website User' and enabled=1 and name not in ('Administrator', 'Guest')"""):
 		
-		summary = prepare_daily_summary(user, unit_post_map, {"subject": subject, "formatted_date": formatted_date,
-			"formatted_event_date": formatted_event_date})
+		summary = prepare_daily_summary(user, posts, events, {
+			"subject": subject, 
+			"formatted_date": formatted_date, 
+			"formatted_event_date": formatted_event_date
+		})
 		
 		if not summary:
 			# no access!
@@ -44,81 +50,102 @@ def send_daily_summary(for_date=None, event_date=None):
 			# for tracking sent status
 			ref_doctype="Profile", ref_docname=user)
 		
+def prepare_daily_summary(user, posts, events, render_opts=None):
+	# show events based on access
+	allowed_events = get_allowed_events(user, events)
 	
-def prepare_daily_summary(user, unit_post_map, render_opts=None):
-	allowed_units = []
-	event_post_map = {
-		"posts": [],
-		"lft": 0,
-		"unit_title": "Events on {}".format(render_opts.get("formatted_event_date")),
-		"public_read": 1
-	}
-	
-	for unit in sorted(unit_post_map, key=lambda u: unit_post_map.get(u, {}).get("lft")):
-		if get_access(unit, profile=user).get("read"):
-			allowed_units.append(unit)
-			event_post_map["posts"] += unit_post_map[unit].get("events") or []
-			
-	if event_post_map["posts"]:
-		allowed_units = ["Events"] + allowed_units
-			
-	if allowed_units:
-		if not render_opts: render_opts = {}
+	if not (posts or allowed_events):
+		return ""
 		
-		render_opts.update({
-			"allowed_units": allowed_units,
-			"unit_post_map": unit_post_map,
-			"event_post_map": event_post_map
+	if not render_opts: render_opts = {}
+	
+	render_opts.update({
+		"posts": posts,
+		"events": allowed_events
+	})
+	
+	return get_summary_template().render(render_opts)
+	
+def get_posts_and_events(for_date, event_date):
+	"""get a dict of posts per unit and unit properties"""
+	# get public posts
+	posts = webnotes.conn.sql("""select *,
+		(select count(pc.name) from `tabPost` pc where pc.parent_post=p.name) as post_reply_count
+		from `tabPost` p
+		where date(creation)=%s and ifnull(parent_post, '')='' and
+		exists(select name from `tabUnit` u where u.name=p.unit and u.public_read=1)
+		order by (p.upvotes + post_reply_count - (timestampdiff(hour, p.creation, %s) / 2)) desc, p.creation desc
+		limit 20""",
+		(for_date, for_date), as_dict=True)
+	
+	if posts:
+		process_posts(posts)
+	
+	# get all events, later filtered by access
+	events = webnotes.conn.sql("""select *,
+		(select count(pc.name) from `tabPost` pc where pc.parent_post=p.name) as post_reply_count
+		from `tabPost` p where is_event=1 and date(event_datetime)=%s and ifnull(parent_post, '')=''
+		order by event_datetime, unit""", (event_date,), as_dict=True)
+	
+	if events:
+		process_posts(events, is_event=True)
+	
+	return posts, events
+
+def process_posts(posts, is_event=False):
+	if not hasattr(webnotes.local, "summary_commons"):
+		webnotes.local.summary_commons = webnotes._dict({
+			"unit": {},
+			"profile": {}
 		})
 		
-		return webnotes.get_template("templates/emails/daily_summary.html").render(render_opts)
-	else:
-		return ""
-	
-def get_unit_post_map(for_date, event_date):
-	"""get a dict of posts per unit and unit properties"""
-	unit_post_map = webnotes._dict()
-	profiles = {}
-
-	# set posts
-	posts = webnotes.conn.sql("""select unit, name, owner, creation,
-		content, picture_url, event_datetime,
-		(select count(pc.name) from `tabPost` pc where pc.parent_post=p.name) as post_reply_count
-		from `tabPost` p where date(creation)=%s and ifnull(parent_post, '')=''
-		order by unit, creation""", (for_date,), as_dict=True)
+	summary_commons = webnotes.local.summary_commons
 		
-	set_posts_in(unit_post_map, posts, "posts", profiles)
-	
-	# set events
-	events = webnotes.conn.sql("""select unit, name, owner, creation,
-		content, picture_url, event_datetime,
-		(select count(pc.name) from `tabPost` pc where pc.parent_post=p.name) as post_reply_count
-		from `tabPost` p where date(event_datetime)=%s and ifnull(parent_post, '')=''
-		order by unit, event_datetime""", (event_date,), as_dict=True)
-
-	set_posts_in(unit_post_map, events, "events", profiles)
-	
-	# update unit info
-	for unit, opts in unit_post_map.items():
-		opts.update(webnotes.conn.get_value("Unit", opts.unit or unit, 
-			["lft", "rgt", "public_read", "unit_title"], as_dict=True) or {})
-		opts.parents = webnotes.conn.sql("""select name, unit_title from tabUnit 
-			where lft < %s and rgt > %s order by lft asc""", (opts.lft, opts.rgt), as_dict=1)
-		
-	return unit_post_map
-	
-def set_posts_in(unit_post_map, posts, key, profiles):
 	for post in posts:
-		if not profiles.get(post.owner):
-			profiles[post.owner] = webnotes.conn.get_value("Profile", post.owner, 
+		# profile data
+		if not summary_commons.profile.get(post.owner):
+			summary_commons.profile[post.owner] = webnotes.conn.get_value("Profile", post.owner, 
 				["first_name", "last_name", "user_image"], as_dict=True)
+				
+		post.update(summary_commons.profile.get(post.owner) or {})
 		
-		post.update(profiles.get(post.owner) or {})
+		# unit data
+		if not summary_commons.unit.get(post.unit):
+			opts = webnotes.conn.get_value("Unit", post.unit, ["lft", "rgt", "unit_title", "unit_type"], 
+				as_dict=True) or {}
+			opts["parents"] = webnotes.conn.sql("""select name, unit_title from tabUnit 
+				where lft < %s and rgt > %s order by lft asc""", (opts.lft, opts.rgt), as_dict=True)
+			summary_commons.unit[post.unit] = opts
 		
+		post.update(summary_commons.unit.get(post.unit) or {})
+		
+		# event data
 		if post.event_datetime:
 			post.event_datetime = get_datetime(post.event_datetime)
-			post.event_display = post.event_datetime.strftime("%a, %d %B, %Y at %I:%M %p") if key=="posts" \
+			post.event_display = post.event_datetime.strftime("%a, %d %B, %Y at %I:%M %p") if not is_event \
 				else post.event_datetime.strftime("at %I:%M %p")
-		
-		unit_post_map.setdefault(post.unit, webnotes._dict()).setdefault(key, []).append(post)
+				
+def get_allowed_events(user, events):
+	unit_access = {}
+	for unit in set([p.unit for p in events]):
+		unit_access[unit] = get_access(unit, profile=user).get("read") or 0
 	
+	allowed_events = []
+	for post in events:
+		if unit_access.get(post.unit):
+			allowed_events.append(post)
+			
+	return allowed_events
+				
+daily_summary_template = None
+def get_summary_template():
+	global daily_summary_template
+	
+	if not daily_summary_template:
+		# load template
+		jenv = webnotes.get_jenv()
+		template_path = os.path.join(os.path.dirname(__file__), "templates", "emails", "daily_summary.html")
+		with open(template_path, "r") as t:
+			daily_summary_template = jenv.from_string(inline_css(t.read()))
+	
+	return daily_summary_template
