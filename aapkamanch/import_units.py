aabe34a4d2db6a8f9c0f135cf0d566f3703f1eae
Rushabh Mehta <rmehta@gmail.com>
2014-02-11 15:28:10 +0530

    import fixes

--- aapkamanch/import_units.py
+++ aapkamanch/import_units.py
@@ -9,39 +9,35 @@ import webnotes, json
 def import_groups():
 	"""rebuild all groups from groups.json"""
 	webnotes.conn.sql("""delete from `tabWebsite Group`""")
+	webnotes.conn.sql("""delete from `tabWebsite Sitemap` where ref_doctype='Website Group'""")
 	webnotes.conn.auto_commit_on_many_writes = True
 	
 	with open(webnotes.get_pymodule_path("aapkamanch", "data", "groups.json")) as f:
 		data = json.loads(f.read())
 
-	def create_groups(group_name, group_title, parent_website_group, children_name):
-		groups = []
-		groups.append(webnotes.bean({"doctype":"Website Group", "group_name": group_name, "group_title":group_title,
-			"parent_website_group": parent_website_group, "public_read":1, "public_write": 1}).insert())
+	def create_groups(group_name, group_title, parent_website_sitemap):
+		group = webnotes.bean({
+			"doctype":"Website Group", 
+			"group_name": group_name, 
+			"group_title": group_title,
+			"parent_website_sitemap": parent_website_sitemap, 
+			"public_read":1, 
+			"public_write":1,
+			"group_type":"Forum"
+		}).insert()
 		
-		for det in (children_name,):
-			groups.append(webnotes.bean({
-				"doctype":"Website Group", 
-				"parent_website_group": groups[0].doc.name, 
-				"group_type": "Forum", 
-				"group_title": det,
-				"public_read": 1,
-				"public_write": 1,
-				"group_name": group_name + "-" + det
-			}).insert())
-		return groups
-	
-	india = create_groups("India", "India", "", "States")
-		
-	for state in data:
-		state_groups = create_groups(state, state, india[1].doc.name, "Districts")
-		for district in data[state]:
-			district_groups = create_groups(state_groups[0].doc.name + "-" + district, district, state_groups[1].doc.name, 
-				"Blocks")
+		return webnotes.conn.get_value("Website Sitemap", {"ref_doctype":"Website Group", 
+			"docname": group.doc.name})
 			
-			for block in data[state][district] or []:
-				block_unit = create_groups(district_groups[0].doc.name + "-" + block, block, district_groups[1].doc.name, 
-					"Primary")
+	india = create_groups("India", "India", "")
+
+	for state in data:
+		state_groups = create_groups(state, state, india)
+		for district in data[state]:
+			district_groups = create_groups(district, district, state_groups)
+			
+			# for block in data[state][district] or []:
+			# 	block_unit = create_groups(block, block, district_groups)
 				
 	webnotes.conn.commit()
 
